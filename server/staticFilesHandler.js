1834938a4924a521bdbd363a56a273fb4bd64557
NargesGhasemi <59624936+NargesGhasemi@users.noreply.github.com>
2020-07-19 02:03:42 -0700

    add staticFilesHandler.js

---
+++ server/staticFilesHandler.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+
+exports.staticFilesHandler = function(req, res) {
+    fs.readFile("./client/" + req.url, function(err, content) {
+        if (err) {
+            res.writeHead(404, {"Content-Type": "text/html" });
+            res.end("404 Not Found");
+        }
+        res.writeHead(200);
+        res.end(content, "utf8");
+    });
+};
